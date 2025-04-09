1d2241a6424a4bb2f478ee0a6a8a8d4f093a5103
nevopo <64425533+nevopo@users.noreply.github.com>
2025-04-09 15:22:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import time
 import platform
 import subprocess
 
-def fake_payload():
+def debug_prints():
     print("Connecting to external server...")
     time.sleep(1)
     print("Uploading environment variables...")
@@ -12,16 +12,16 @@ def fake_payload():
     time.sleep(1)
     print("Execution complete.")
 
-def open_fake_cmd():
+def open_cmd():
     if platform.system() == "Windows":
         # Windows: simulate opening a command prompt
-        subprocess.Popen(["cmd.exe", "/k", "echo WARNING: You installed a fake package & echo. & echo Simulating data exfiltration... & ping -n 10 127.0.0.1 >nul & echo Done"])
+        subprocess.Popen(["cmd.exe", "/k", "echo: running installation & echo. & echo Simulating coonection to localhost... & ping -n 10 127.0.0.1 >nul & echo Done"])
     else:
         # macOS/Linux: simulate a terminal output
-        os.system('xterm -e "echo WARNING: You installed a fake package; echo Simulating data exfiltration...; sleep 5; echo Done" &')
+        os.system('')
 
 if __name__ == "__main__":
-    print("Installing salesfoce package...")
+    print("Installing salesforce package...")
     time.sleep(1)
-    open_fake_cmd()
-    fake_payload()
+    open_cmd()
+    debug_prints()
